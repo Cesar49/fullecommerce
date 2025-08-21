5dc544fb3fb7deba992fd1824e072d32777d1927
CesarEsqueda <cesaresquedadeveloper@gmail.com>
2025-08-21 10:16:10 -0400

    first commit video 1 instalacion y lista usuarios dashboard

---
+++ database/seeders/AdminSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class AdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::create([
+            'name' => 'Cesar Esqueda',
+            'username' => 'admin',
+            'phone' => '04243317094',
+            'address' => 'San Fernando',
+            'avatar' => 'avatar.png',
+            'email' => 'cesaresquedadeveloper@gmail.com',
+            'password' => Hash::make('12345678'),
+            'role' => 'admin',
+        ]);
+    }
+}
